e6de48f26248c6fdbe4ac7d32499833d3b78783b
BruceGliff <ilyaawaA@yandex.ru>
2021-09-10 04:57:30 -0400

    Initialize glfw window
    Add debug flag for future using

--- src/main.cc
+++ src/main.cc
@@ -1,31 +1,59 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <cassert>
 
 #include "decoy/decoy.h"
 
 class HelloTriangleApplication {
+  GLFWwindow * m_Window {};
+
+  unsigned const m_Width {1600};
+  unsigned const m_Height {900};
+
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);  
+    m_Window = glfwCreateWindow(m_Width, m_Height, "Vulkan", nullptr, nullptr);
+    assert(m_Window && "Window initializating falis!");
+  }
+
   void initVulkan() {}
 
-  void mainLoop() {}
+  void mainLoop() {
+    while (!glfwWindowShouldClose(m_Window)) {
+      glfwPollEvents();
+    }
+  }
 
-  void cleanup() {}
+  void cleanup() {
+    glfwDestroyWindow(m_Window);
+
+    glfwTerminate();
+  }
 };
 
 int main() {
-  HelloTriangleApplication app;
-
+  #ifdef Debug
+    std::cout << "Debug\n";
+  #endif // Debug
   Decoy::Dump();
+
+  HelloTriangleApplication app;
   try {
     app.run();
   } catch (const std::exception &e) {
