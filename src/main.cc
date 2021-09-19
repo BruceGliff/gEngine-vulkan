ec1aeb16a4d1ab932a341cd908228856b20a9e4a
BruceGliff <ilyaawaA@yandex.ru>
2021-09-19 05:46:43 -0400

    instance creation

--- src/main.cc
+++ src/main.cc
@@ -14,6 +14,9 @@ class HelloTriangleApplication {
   unsigned const m_Width {1600};
   unsigned const m_Height {900};
 
+  // An instance needed for connection between app and VkLibrary
+  // And adds a detailes about app to the driver
+  VkInstance m_instance {};
 public:
   void run() {
     initWindow();
@@ -32,15 +35,55 @@ private:
     assert(m_Window && "Window initializating falis!");
   }
 
-  void initVulkan() {}
+  void initVulkan() {
+    createInstance();
+  }
+
+  void createInstance() {
+    // Some optional information to pass to driver
+    // for some optimizations
+    VkApplicationInfo appInfo {
+      .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+      .pApplicationName = "Hello triangle",
+      .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+      .pEngineName = "No Engine",
+      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+      .apiVersion = VK_API_VERSION_1_0
+    };
+
+    // TODO check for an extensions
+    uint32_t glfwExtensionCount {0};
+    char const ** glfwExtensions {};
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // Required struct tells the Vulkan driver whick global
+    // extension and validation level to use
+    VkInstanceCreateInfo createInfo {
+      .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+      .pApplicationInfo = &appInfo,
+      .enabledLayerCount = 0,
+      .enabledExtensionCount = glfwExtensionCount,
+      .ppEnabledExtensionNames = glfwExtensions
+    };
+
+    // TODO make macros vkSafeCall()
+    if (vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+    
+  }
+
 
   void mainLoop() {
     while (!glfwWindowShouldClose(m_Window)) {
       glfwPollEvents();
+      break;
     }
   }
 
   void cleanup() {
+    vkDestroyInstance(m_instance, nullptr);
+
     glfwDestroyWindow(m_Window);
 
     glfwTerminate();
