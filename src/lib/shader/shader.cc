748b03ca421bae62ae852218446e1afd16f8ffd2
BruceGliff <ilyaawaA@yandex.ru>
2022-05-08 14:43:04 -0400

    createGraphicPipeline - C++.

--- src/lib/shader/shader.cc
+++ src/lib/shader/shader.cc
@@ -5,16 +5,20 @@
 
 #include <cassert>
 
-std::vector<char> Shader::readFile(std::string_view PathToShader) {
+std::vector<uint32_t> Shader::readFile(std::string_view PathToShader) {
   std::ifstream Kernel{PathToShader.data(), std::ios::binary};
   if (!Kernel.is_open()) {
     std::cerr << "Cannot open file: " << PathToShader << '\n';
     return {};
   }
 
-  std::noskipws(Kernel);
-  std::istreambuf_iterator<char> Begin{Kernel}, End;
-  return std::vector<char>{Begin, End};
+  Kernel.seekg(0, std::ios_base::end);
+  std::size_t const Size = Kernel.tellg();
+  assert(Size % 4 == 0 && "SPIRV size has to be 4-multiple");
+  Kernel.seekg(0, std::ios_base::beg);
+  std::vector<uint32_t> SPIRV(Size / sizeof(uint32_t));
+  Kernel.read((char *)&SPIRV[0], Size);
+  return SPIRV;
 };
 
 Shader::Shader(std::string_view PathToShader)
@@ -22,15 +26,6 @@ Shader::Shader(std::string_view PathToShader)
 
 // Seems like defailt allocator already.
 // SPIR-V declares that binary file has to be multiple by 4byte.
-uint32_t const *Shader::getRawData() {
-  return reinterpret_cast<uint32_t const *>(RawData.data());
-}
+std::vector<uint32_t> const &Shader::getSPIRV() const { return RawData; }
 
-uint32_t const *Shader::getRawData() const {
-  return reinterpret_cast<uint32_t const *>(RawData.data());
-}
-
-uint32_t Shader::getSize() const {
-  assert(RawData.size() % 4 == 0);
-  return RawData.size();
-}
+uint32_t Shader::getSize() const { return RawData.size() * 4; }
