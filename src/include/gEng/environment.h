9e2d1a4645bedd10fb0b6930e087f27cd84a66fe
Ilya Andreev <ilya.andreev@syntacore.com>
2022-09-15 10:47:09 -0400

    SysEnv now handles options
    Added option parser from boost in SysEnv

--- src/include/gEng/environment.h
+++ src/include/gEng/environment.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <boost/program_options.hpp>
+
+#include <cstdint>
 #include <filesystem>
+#include <optional>
 #include <string>
 
 namespace fs {
@@ -10,14 +14,19 @@ using path = std::filesystem::path;
 namespace gEng {
 
 struct SysEnv {
-  SysEnv(char const *);
+  SysEnv(int argc, char *argv[]);
 
   std::string getPathStr() const;
   std::string getFilenameStr() const;
   operator fs::path const &() const;
   operator fs::path &();
 
+  bool getHelp() const;
+  std::optional<uint32_t> getFramesLimit() const;
+
 private:
+  boost::program_options::options_description Desc{"Allowed options"};
+  boost::program_options::variables_map Opts;
   fs::path BinPath;
   fs::path FileName;
 };
